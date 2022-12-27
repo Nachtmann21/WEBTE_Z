43e4cd62ccd43178c52148f3f57dd50fc970267c
Martin Nachtmann <xnachtmann@stuba.sk>
2022-12-27 14:04:15 +0100

    Working on generating mines

--- files/js/script.js
+++ files/js/script.js
@@ -1,4 +1,17 @@
-console.log("connected")
+console.log("connected");
+
+var currentLevel = 0;
+var JSONdata;
+
+// Zatial tunak citame ten JSON
+fetch('./files/data/easy.json')
+    .then(response => {
+    return response.json();
+  }).then(data => {    
+    JSONdata = data;
+  }).catch(err => {
+    console.log("JSON error " + err);
+});
 
 const mainMenu = document.getElementById('main-menu');
 const gameBoard = document.getElementById('game-board');
@@ -6,48 +19,77 @@ const easyButton = document.getElementById('easy');
 const hardButton = document.getElementById('hard');
 const quitButton = document.getElementById('quit');
 var difficulty = 'easy';
+var gameOver = false;
 
-// Show the main menu and hide the game board
-function showMainMenu() {
-  mainMenu.style.display = 'flex';
-  gameBoard.style.display = 'none';
+function mainGameLoop(){
+    showGameBoard();
+    generateBoard();
+
 }
 
-// Show the game board and hide the main menu
-function showGameBoard() {
-  mainMenu.style.display = 'none';
-  gameBoard.style.display = 'grid';
-  generateGameBoard();
-}
-
-function generateGameBoard() {
-
-    let numberOfSquares = difficulty === 'easy' ? 6 : 30;
+function generateBoard() {
+    let numberOfSquares = difficulty === 'easy' ? 6 : 12;
 
     gameBoard.style.gridTemplateColumns = `repeat(${numberOfSquares}, 1fr)`;
     gameBoard.style.gridTemplateRows = `repeat(${numberOfSquares}, 1fr)`;
     
-    // Generate the squares
-    for (let i = 0; i < numberOfSquares*numberOfSquares; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.style.width = `95%`;
-        square.style.height = `95%`;
-        gameBoard.appendChild(square);
+    // Fetch data(mine positions) from JSON
+    // Generate the squares according to JSON rules
+    generateTiles(numberOfSquares);
+}
+
+function generateTiles(numberOfSquares) {
+    for(let x = 0; x < numberOfSquares; x++) {
+        for(let y = 0; y < numberOfSquares; y++){
+            const tile = new Tile(x, y);
+            gameBoard.appendChild(tile);
+            console.log(tile.id);
+        }
     }
 }
 
+class Tile {
+    constructor(x, y) {
+        const tile = document.createElement('div');
+        tile.classList.add('tile');
+        tile.style.width = `95%`;
+        tile.style.height = `95%`;
+        return tile;
+    }
+}
+
+function determineTileType(tile, x, y) {
+    const tileCoords = [x, y];
+    const bombCoords = JSONdata[currentLevel].bombCoords;
+    tile.innerHTML = "0";
+    console.log("Json coords: " + bombCoords);
+}
+
+// Show the main menu and hide the game board
+function showMainMenu() {
+    mainMenu.style.display = 'flex';
+    gameBoard.style.display = 'none';
+  }
+  
+  // Show the game board and hide the main menu
+  function showGameBoard() {
+    mainMenu.style.display = 'none';
+    gameBoard.style.display = 'grid';
+  }
+
 easyButton.addEventListener('click', function() {
     difficulty = 'easy';
-    showGameBoard();
+    mainGameLoop();
 });
 
 hardButton.addEventListener('click', function() {
     difficulty = 'hard';
-    showGameBoard();
+    mainGameLoop();
 });
 
 quitButton.addEventListener('click', function() {
     gameBoard.innerHTML = '';
     showMainMenu();
 });
+
+
